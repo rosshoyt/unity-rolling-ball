6348b19f8e33c34961697cb6f76b112f95fd45a2
Ross Hoyt <rosshoytmusic@gmail.com>
2020-06-20 16:50:33 -0700

    Implemented interactive score

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -16,14 +16,50 @@ public class AudioManager : MonoBehaviour
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
         }
     }
 
     public void Play(string name)
     {
-        Debug.Log("Playing sound " + name);
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        s.source.Play();
+        FindSound(name).source.Play();
+    }
+    public void Stop(string name)
+    {
+        FindSound(name).source.Stop();
     }
 
+    public void SetVolume(string name, float vol)
+    {
+        //Debug.Log("Setting volume to " + vol.ToString());
+        Sound s = FindSound(name);
+        s.volume = vol;
+        s.source.volume = vol;
+    }
+
+    /**
+     * Changes volume of a sound by specified amount
+     * @param vol float from 1.0f to (-1.0f)
+     */ 
+    public void ChangeVolume(string name, float vol)
+    {
+        Sound s = FindSound(name);
+        float newVol = s.volume + vol;
+        Debug.Log("Changing volume to " + newVol.ToString());
+        s.volume = newVol;
+        s.source.volume = newVol;
+    }
+
+    public float GetVolume(string name)
+    {
+        //Sound s = FindSound(name);//.source.volume;
+        return Array.Find(sounds, sound => sound.name == name).source.volume;
+        //Array.Find(sounds, sound => sound.name == name);
+
+    }
+
+    private Sound FindSound(string name)
+    {
+        return Array.Find(sounds, sound => sound.name == name);
+    }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,13 @@ public class PlayerController : MonoBehaviour
 
     private int count;
 
+    // refactor to be set to size of PickUp prefab group
+    private const int TOTAL_COUNT = 12;
+    private const int N_VOLUME_INCREMENTS = TOTAL_COUNT / 2;
+    private const float VOLUME_INCREMENT = 1f / N_VOLUME_INCREMENTS;
+
+    private AudioManager audioManager;
+
 
     void Start()
     {
@@ -20,6 +27,14 @@ public class PlayerController : MonoBehaviour
         count = 0;
         SetCountText();
         winText.text = "";
+        audioManager = FindObjectOfType<AudioManager>();
+
+
+
+        // start score (only layer 1 is audible)
+        audioManager.Play("Music1_Layer1");
+        audioManager.Play("Music1_Layer2");
+        audioManager.Play("Music1_Layer3");
     }
 
     void FixedUpdate()
@@ -32,18 +47,59 @@ public class PlayerController : MonoBehaviour
     
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Pick Up"))
+        if (other.gameObject.CompareTag("Pick_Up"))
         {
             other.gameObject.SetActive(false);
             count++;
             SetCountText();
-            FindObjectOfType<AudioManager>().Play("Pickup");
+            
+            // SFX/Score
+            PlaySound("Pick_Up");
+            if (TOTAL_COUNT > 1)
+            {
+                if (count <= TOTAL_COUNT / 2)
+                {
+                    //float currentVol = audioManager.GetVolume("Music1_Layer2");
+                    audioManager.ChangeVolume("Music1_Layer2",  VOLUME_INCREMENT);
+                    
+                }
+                else if (count < TOTAL_COUNT)
+                {
+                    audioManager.ChangeVolume("Music1_Layer3", VOLUME_INCREMENT);
+                }
+                else if (count == TOTAL_COUNT)
+                {
+                    StopSound("Music1_Layer1");
+                    StopSound("Music1_Layer2");
+                    StopSound("Music1_Layer3");
+                    PlaySound("Music2_FullMix");
+                    PlaySound("Success");
+                }
+            }
+
         }
     }
     private void SetCountText()
     {
-        countText.text = "Count: " + count.ToString();
-        if (count >= 12)
+        countText.text = "Count: " + count.ToString() + "/" + TOTAL_COUNT.ToString();
+        
+        if (count >= TOTAL_COUNT)
             winText.text = "You Win!";
+    }
+
+    // utility method that plays a sound using AudioManager
+    private void PlaySound(string sound)
+    {
+        audioManager.Play(sound);
+    }
+
+    private void StopSound(string sound)
+    {
+        FindObjectOfType<AudioManager>().Stop(sound);
+    }
+    // TODO create AudioManager field
+    private void SetSoundVolume(string sound, float vol)
+    {
+        FindObjectOfType<AudioManager>().SetVolume(sound, vol);
     }
 }
